c1042446a26657e0bf750876e6286b130364372e
kacper <kacper.perz@marviq.com>
2025-12-10 16:58:40 +0100

    Moved chart definition to FXML

--- src/main/java/com/prezyk/controller/ChartsController.java
+++ src/main/java/com/prezyk/controller/ChartsController.java
@@ -5,7 +5,6 @@ import com.prezyk.md.Molecules;
 import com.prezyk.event.SimulationCalculationsFinishedEvent;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.layout.Pane;
@@ -18,22 +17,12 @@ public class ChartsController {
     @FXML
     Pane chartPane;
 
-    ScatterChart<Number, Number> figure;
+    @FXML
+    ScatterChart<Number, Number> chart;
 
     public void initialize() {
         EventDispatcher eventDispatcher = EventDispatcher.getInstance();
         eventDispatcher.registerEventHandler(SimulationCalculationsFinishedEvent.class, this::simulationCalculationsFinishedEventHandler);
-        initFigure();
-    }
-
-    private void initFigure() {
-        NumberAxis x = new NumberAxis();
-        NumberAxis y = new NumberAxis();
-        figure = new ScatterChart<>(x, y);
-        figure.setLegendVisible(true);
-        figure.setMinSize(chartPane.getWidth(), chartPane.getHeight());
-        if (chartPane.getChildren().isEmpty())
-            chartPane.getChildren().add(figure);
     }
 
     private void reloadChartData(Molecules molecules) {
@@ -44,37 +33,44 @@ public class ChartsController {
 
     private void resetChartsIfNeeded(ChartMapper chartMapper) {
 
-        List<String> currentChartNames = figure.getData()
-                                               .stream()
-                                               .map(XYChart.Series::getName)
-                                               .collect(Collectors.toList());
-        if (chartMapper.getNames().size() != currentChartNames.size() || !chartMapper.getNames().containsAll(currentChartNames) ) {
-            figure.getData().clear();
+        List<String> currentChartNames = chart.getData()
+                                              .stream()
+                                              .map(XYChart.Series::getName)
+                                              .collect(Collectors.toList());
+        if (chartMapper.getNames()
+                       .size() != currentChartNames.size() || !chartMapper.getNames()
+                                                                          .containsAll(currentChartNames)) {
+            chart.getData()
+                 .clear();
             initCharts(chartMapper);
         }
     }
 
     private void initCharts(ChartMapper chartMapper) {
-        chartMapper.getNames().forEach(chartName -> {
-            XYChart.Series<Number, Number> series = new XYChart.Series<>();
-            series.setName(chartName);
-            figure.getData().add(series);
-        });
+        chartMapper.getNames()
+                   .forEach(chartName -> {
+                       XYChart.Series<Number, Number> series = new XYChart.Series<>();
+                       series.setName(chartName);
+                       chart.getData()
+                            .add(series);
+                   });
     }
 
     private void loadAllCharts(ChartMapper chartMapper) {
-        figure.getData()
-              .forEach(series -> loadChart(series, chartMapper.getTimeSeries(series.getName()), chartMapper.getTimePoints()));
+        chart.getData()
+             .forEach(series -> loadChart(series, chartMapper.getTimeSeries(series.getName()), chartMapper.getTimePoints()));
     }
 
     private void loadChart(XYChart.Series<Number, Number> series, double[] timeSeries, double[] timePoints) {
-        series.getData().clear();
+        series.getData()
+              .clear();
 
         if (timeSeries.length != timePoints.length)
             throw new IllegalArgumentException("Time series should have the same amount of points as time points.");
 
         for (int i = 0; i < timePoints.length; i++) {
-            series.getData().add(new XYChart.Data<>(timePoints[i], timeSeries[i]));
+            series.getData()
+                  .add(new XYChart.Data<>(timePoints[i], timeSeries[i]));
         }
     }
 
